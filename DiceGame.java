f30391cdbadd0f05eea86cfe385bfa1a93c8c3eb
SakuraiAyato <aplistia.99@gmail.com>
2023-06-15 17:55:27 +0900

    Won/lost message modified

--- DiceGame.java
+++ DiceGame.java
@@ -18,9 +18,9 @@ public class DiceGame {
         System.out.println("Total value: " + totalValue);
 
 	if (total > 7) {
-            System.out.println("You won!");
+            System.out.println(name + " won!");
         } else {
-            System.out.println("You lost.");
+            System.out.println(name + " lost.");
         }
     }
 
